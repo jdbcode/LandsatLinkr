8c469bba4bd49caf408bca2cad2020d6a726a1a7
jdbcode <jstnbraaten@gmail.com>
2015-08-13 15:29:21 -0700

    only uses bands 2:7 now instead of 1:7

--- R/olisr2tc.r
+++ R/olisr2tc.r
@@ -10,7 +10,7 @@
 #' @export
 
 
-olisr2tc = function(oli_file,bcoef,gcoef,wcoef,mode){
+olisr2tc = function(oli_file,bcoef,gcoef,wcoef,mode,overwrite=F){
   
   if(mode == "calibrate"){
     dir = substr(dirname(oli_file),1,nchar(dirname(oli_file))-12)
@@ -24,33 +24,38 @@ olisr2tc = function(oli_file,bcoef,gcoef,wcoef,mode){
     tcafile = sub("l8sr.tif","tca.tif", oli_file)
   }
   
+  check = file_check(oli_file,"l8sr_tc.tif",overwrite)
+  if(check == 0){return(0)}
+  
   #tasseled cap
-  ref = raster(finalstack, 1)
-  b1 = as.matrix(raster(finalstack, 1))
-  b2 = as.matrix(raster(finalstack, 2))
-  b3 = as.matrix(raster(finalstack, 3))
-  b4 = as.matrix(raster(finalstack, 4))
-  b5 = as.matrix(raster(finalstack, 5))
-  b6 = as.matrix(raster(finalstack, 6))
-  b7 = as.matrix(raster(finalstack, 7))
+  b2 = as.matrix(raster(oli_file, 2))
+  b3 = as.matrix(raster(oli_file, 3))
+  b4 = as.matrix(raster(oli_file, 4))
+  b5 = as.matrix(raster(oli_file, 5))
+  b6 = as.matrix(raster(oli_file, 6))
+  b7 = as.matrix(raster(oli_file, 7))
   
-  bright = (b1*bcoef[1])+(b2*bcoef[2])+(b3*bcoef[3])+(b4*bcoef[4])+(b5*bcoef[5])+(b6*bcoef[6])+(b6*bcoef[7])
-  green = (b1*gcoef[1])+(b2*gcoef[2])+(b3*gcoef[3])+(b4*gcoef[4])+(b5*gcoef[5])+(b6*gcoef[6])+(b6*gcoef[7])
-  wet = (b1*wcoef[1])+(b2*wcoef[2])+(b3*wcoef[3])+(b4*wcoef[4])+(b5*wcoef[5])+(b6*wcoef[6])+(b6*wcoef[7])
+  bright = (b2*bcoef[1])+(b3*bcoef[2])+(b4*bcoef[3])+(b5*bcoef[4])+(b6*bcoef[5])+(b7*bcoef[6])
+  green = (b2*gcoef[1])+(b3*gcoef[2])+(b4*gcoef[3])+(b5*gcoef[4])+(b6*gcoef[5])+(b7*gcoef[6])
+  wet = (b2*wcoef[1])+(b3*wcoef[2])+(b4*wcoef[3])+(b5*wcoef[4])+(b6*wcoef[5])+(b7*wcoef[6])
   
-  b1=b2=b3=b4=b5=b6=0
+  b2=b3=b4=b5=b6=b7=0
 
-  tcb = matrix_to_raster(finalstack,bright)
-  tcg = matrix_to_raster(finalstack,green)
-  tcw = matrix_to_raster(finalstack,wet)
+
+  tcb = matrix_to_raster(oli_file,bright)
+  tcg = matrix_to_raster(oli_file,green)
+  tcw = matrix_to_raster(oli_file,wet)
   wet=0  
   tca = atan(green/bright) * (180/pi) * 100
   bright=green=0  
-  tca = matrix_to_raster(finalstack,tca)
   
-  temptcb = file.path(tempdir,paste(outbase,"_temptcb.tif",sep=""))
-  temptcg = file.path(tempdir,paste(outbase,"_temptcg.tif",sep=""))
-  temptcw = file.path(tempdir,paste(outbase,"_temptcw.tif",sep=""))
+  tca = matrix_to_raster(oli_file,tca)
+  
+  outbase = substr(basename(oli_file),1,16)
+  outdir = dirname(oli_file)
+  temptcb = sub("l8sr.tif","_temptcb.tif", oli_file)
+  temptcg = sub("l8sr.tif","_temptcg.tif", oli_file)
+  temptcw = sub("l8sr.tif","_temptcw.tif", oli_file)
   projection(tcb) = set_projection(tcfile)
   projection(tcg) = set_projection(tcfile)
   projection(tcw) = set_projection(tcfile)
@@ -65,7 +70,7 @@ olisr2tc = function(oli_file,bcoef,gcoef,wcoef,mode){
   writeGDAL(tc, temptcw, drivername = "GTiff", type = "Int16", mvFlag = -32768, options="INTERLEAVE=BAND")
   
   bands = c(temptcb,temptcg,temptcw)
-  temptcs = file.path(tempdir,paste(outbase,"_temptcstack.vrt",sep=""))
+  temptcs = sub("l8sr.tif","_temptcstack.vrt", oli_file) 
   gdalbuildvrt(gdalfile=bands, output.vrt = temptcs, separate=T) #, tr=c(reso,reso)
   gdal_translate(src_dataset=temptcs, dst_dataset=tcfile, of = "GTiff", co="INTERLEAVE=BAND")
   
@@ -75,39 +80,7 @@ olisr2tc = function(oli_file,bcoef,gcoef,wcoef,mode){
   writeGDAL(tc, tcafile, drivername = "GTiff", type = "Int16", mvFlag = -32768, options="INTERLEAVE=BAND")
   
   #delete temporary files
-  unlink(tempdir, recursive=T, force=T)
+  unlink(c(temptcb,temptcg,temptcw,temptcs))
   
   return(1)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
