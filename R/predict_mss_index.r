e47449e1091789987904e3bdf87d4bc4a37bdf96
jdbcode <jstnbraaten@gmail.com>
2015-08-17 12:51:09 -0700

    removed commented sections

--- R/predict_mss_index.r
+++ R/predict_mss_index.r
@@ -8,15 +8,12 @@
 
 
 predict_mss_index = function(refsamp, b1samp, b2samp, b3samp, b4samp, mss_sr_file, ref_file, index, outsampfile, samplen){  
-  #make a dateframe of the training sample
 
-  
   tbl = data.frame(mss_img = rep(basename(mss_sr_file),samplen),
                    ref_img = rep(basename(ref_file),samplen),
                    index = rep(index,samplen),
                    refsamp,b1samp,b2samp,b3samp,b4samp)
   final = tbl[complete.cases(tbl),]
-  
   
   #create a multivariable linear model
   model = rlm(refsamp ~ b1samp + b2samp + b3samp + b4samp, data=final)
@@ -35,22 +32,8 @@ predict_mss_index = function(refsamp, b1samp, b2samp, b3samp, b4samp, mss_sr_fil
   r = cor(final$refsamp, final$singlepred)
   coef = rlm(final$refsamp ~ final$singlepred)
   
-#   g = ggplot(final, aes(singlepred, refsamp)) +
-#     stat_binhex(bins = 100)+
-#     scale_fill_gradientn(name = "Count", colours = rainbow(7))+
-#     xlab(paste(basename(mss_sr_file),index)) +
-#     ylab(paste(basename(ref_file),index)) +
-#     ggtitle(paste(index,"linear regression: slope =",paste(signif(coef$coefficients[2], digits=3),",",sep=""),
-#                   "y Intercept =",paste(round(coef$coefficients[1], digits=3),",",sep=""),
-#                   "r =",signif(r, digits=3))) +
-#     theme(plot.title = element_text(size = 12)) +
-#     geom_smooth(method="rlm", colour = "black", se = FALSE) + 
-#     coord_fixed(ratio = 1)+
-#     theme_bw()
-  
   pngout = sub("samp.csv", "plot.png",outsampfile)
   png(pngout,width=700, height=700)
-  #print(g)
   title = paste(index,"linear regression: slope =",paste(signif(coef$coefficients[2], digits=3),",",sep=""),
                 "y Intercept =",paste(round(coef$coefficients[1], digits=3),",",sep=""),
                 "r =",signif(r, digits=3))
@@ -63,6 +46,6 @@ predict_mss_index = function(refsamp, b1samp, b2samp, b3samp, b4samp, mss_sr_fil
   
   #return the information
   coef = data.frame(yint,b1c,b2c,b3c,b4c)
-  tbls = list(coef,final) #,g
+  tbls = list(coef,final)
   return(tbls)
 }
