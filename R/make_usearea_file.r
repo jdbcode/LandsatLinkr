f401e606c988569d2fd149220d976022c8aef1f7
jdbcode <jstnbraaten@gmail.com>
2015-08-17 12:48:53 -0700

    added OLI dir search and got rid of rfiles variable since the resolution should always be 30 - don't need to find it on the fly

--- R/make_usearea_file.r
+++ R/make_usearea_file.r
@@ -14,13 +14,10 @@ make_usearea_file = function(dir, outfile, xmx, xmn, ymx, ymn){
   projfiles = list.files(path = file.path(dir[1], "images"), pattern="proj.txt", full.names=T, recursive=T)
   if(length(projfiles) == 0){projfiles = list.files(path = file.path(dir[2], "images"), pattern="proj.txt", full.names=T, recursive=T)}
   if(length(projfiles) == 0){projfiles = list.files(path = file.path(dir[3], "images"), pattern="proj.txt", full.names=T, recursive=T)} 
-    
-  rfiles = list.files(path = file.path(dir[1], "images"), pattern=".tif$", full.names=T, recursive=T)
-  if(length(rfiles) == 0){rfiles = list.files(path = file.path(dir[2], "images"), pattern=".tif$", full.names=T, recursive=T)}
-  if(length(rfiles) == 0){rfiles = list.files(path = file.path(dir[3], "images"), pattern=".tif$", full.names=T, recursive=T)}
+  if(length(projfiles) == 0){projfiles = list.files(path = file.path(dir[4], "images"), pattern="proj.txt", full.names=T, recursive=T)} 
   
+   
   crs = readLines(projfiles[1])
-  #res = xres(raster(rfiles[1]))
   res = 30
   r = raster(xmn=xmn, xmx=xmx, ymn=ymn, ymx=ymx, crs=crs, res=res)
   r[] = 1
