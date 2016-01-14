1a7546ea6012215ba74c9785d30ed83756a23e05
jdbcode <jstnbraaten@gmail.com>
2016-01-14 12:27:49 -0800

    checks dem extent against mss image set union to make sure there is full overlap. also crops the dem, slope, and aspect files to the mss image set union

--- R/prepare_images.r
+++ R/prepare_images.r
@@ -97,40 +97,94 @@ prepare_images = function(scenedir, demfile=NULL, proj="default", process=seq(1:
     reso = xres(template)
     demname = paste(info$wrstype,"_",info$ppprrr,"_",reso,"m","_dem.tif",sep="")
     newdem = file.path(topodir,demname)
+    tempdem = sub("dem.tif","temp_dem.tif",newdem)
     newslope = file.path(topodir,sub("dem","slope",demname))
     newasp = file.path(topodir,sub("dem","aspect",demname))
     newill = file.path(topodir,sub("dem","illumination",demname))
+    #extname = paste(info$wrstype,"_",info$ppprrr,"_mss_image_set_union_extent.csv",sep="")
+    #extfile = file.path(topodir,extname)
     s_srs = projection(template)
     t_srs = set_projection(examplefile)
     
-    havedem = file.exists(newdem)
-    if(havedem == T & overwrite == T | havedem == F){
-      if(havedem == T){unlink(newdem)}
-      gdalwarp(srcfile=demfile,dstfile=newdem,
-               s_srs=s_srs,t_srs=t_srs, tr=c(60,60), dstnodata=-32768, ot="Int16")
+    #havedem = file.exists(newdem)
+    demfiles = list.files(topodir,"dem",full.names=T)
+    #if(havedem == T & overwrite == T | havedem == F){
+    unlink(demfiles)
+    gdalwarp(srcfile=demfile,dstfile=tempdem,
+             s_srs=s_srs,t_srs=t_srs, tr=c(60,60), dstnodata=-32768, ot="Int16")
+    #}
+    
+    extholder = matrix(ncol = 4, nrow=length(files))
+    print("...Making sure DEM is large enough")
+    print("......Getting MSS image extents")
+    for(i in 1:length(files)){ 
+      print(i)
+      img = raster(files[i])
+      ext = extent(img)
+      extholder[i,1] = ext@xmin
+      extholder[i,2] = ext@xmax
+      extholder[i,3] = ext@ymin
+      extholder[i,4] = ext@ymax
+    }
+    adj=1500
+    xmin = min(extholder[,1]) - adj
+    xmax = max(extholder[,2]) + adj
+    ymin = min(extholder[,3]) - adj
+    ymax = max(extholder[,4]) + adj
+    
+    dem = raster(tempdem)
+    demext = extent(dem)
+    
+    xminokay = demext@xmin <= xmin
+    xmaxokay = demext@xmax >= xmax
+    yminokay = demext@ymin <= ymin
+    ymaxokay = demext@ymax >= ymax
+    
+    print(paste(".........DEM x minimum is okay:",xminokay))
+    print(paste(".........DEM x maximum is okay:",xmaxokay))
+    print(paste(".........DEM y minimum is okay:",yminokay))
+    print(paste(".........DEM y maximum is okay:",ymaxokay))
+    
+    if(sum(c(xminokay,xmaxokay,yminokay,ymaxokay)) != 4){
+      print("Error - Please make sure DEM has minimum dimensions:")
+      print("x minimum:", xmin)
+      print("x maximum:", xmax)
+      print("y minimum:", ymin)
+      print("y maximum:", ymax)
+      print("For projection:",t_srs)
+      return("Stopping LLR")
     }
     
+    #crop the dem
+    print("...Croppping the DEM to MSS image set union plus 25 pixel buffer")
+    gdal_translate(src_dataset=tempdem, dst_dataset=newdem, projwin=c(xmin,ymax,xmax,ymin))
+    tempfiles = list.files(topodir, "temp", full.names=T)
+    unlink(tempfiles)
     dem = raster(newdem)
     
-    haveslope = file.exists(newslope)
-    if(haveslope == T & overwrite == T | haveslope == F){
-      if(haveslope == T){unlink(newslope)}
-      print("...Preparing Slope")
-      img = terrain(dem, opt="slope")
-      projection(img) = set_projection(examplefile)
-      img = as(img, "SpatialGridDataFrame")
-      writeGDAL(img, newslope, drivername = "GTiff", type = "Float32", options="INTERLEAVE=BAND") #, mvFlag = -32768
-    }
+    #making slope
+    #haveslope = file.exists(newslope)
+    slopefiles = list.files(topodir,"slope",full.names=T)
+    #if(haveslope == T & overwrite == T | haveslope == F){
+    unlink(slopefiles)
+    print("...Preparing Slope")
+    img = terrain(dem, opt="slope")
+    projection(img) = set_projection(examplefile)
+    img = as(img, "SpatialGridDataFrame")
+    writeGDAL(img, newslope, drivername = "GTiff", type = "Float32", options="INTERLEAVE=BAND") #, mvFlag = -32768
+    #}
     
-    haveasp = file.exists(newasp)
-    if(haveasp == T & overwrite == T | haveasp == F){
-      if(haveasp == T){unlink(newasp)}
-      print("...Preparing Aspect")
-      img = terrain(dem, opt="aspect")
-      projection(img) = set_projection(examplefile)
-      img = as(img, "SpatialGridDataFrame")
-      writeGDAL(img, newasp, drivername = "GTiff", type = "Float32", options="INTERLEAVE=BAND") #, mvFlag = -32768  
-    }
+    #making slope aspect
+    #haveasp = file.exists(newasp)
+    aspfiles = list.files(topodir,"aspect",full.names=T)
+    #if(haveasp == T & overwrite == T | haveasp == F){
+    unlink(aspfiles)
+    print("...Preparing Aspect")
+    img = terrain(dem, opt="aspect")
+    projection(img) = set_projection(examplefile)
+    img = as(img, "SpatialGridDataFrame")
+    writeGDAL(img, newasp, drivername = "GTiff", type = "Float32", options="INTERLEAVE=BAND") #, mvFlag = -32768  
+    #}
     
     img=0
     
