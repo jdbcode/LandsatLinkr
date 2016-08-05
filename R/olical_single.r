c3f091c95dca9ac961bb207c5d1135ad2f60fb1d
jdbcode <jstnbraaten@gmail.com>
2016-08-05 09:51:47 -0700

    added external function "predict_oli_index" as internal function

--- R/olical_single.r
+++ R/olical_single.r
@@ -17,6 +17,37 @@ olical_single = function(oli_file, tm_file, overwrite=F){
     return(int)
   }
   
+  predict_oli_index = function(tbl, outsampfile){  
+    
+    #create a multivariable linear model
+    model = rlm(refsamp ~ b2samp + b3samp + b4samp + b5samp + b6samp + b7samp, data=tbl) #
+    
+    tbl$singlepred = round(predict(model))
+    write.csv(tbl, outsampfile, row.names=F)
+    
+    #plot the regression
+    r = cor(tbl$refsamp, tbl$singlepred)
+    coef = rlm(tbl$refsamp ~ tbl$singlepred)
+    
+    pngout = sub("samp.csv", "plot.png",outsampfile)
+    png(pngout,width=700, height=700)
+    title = paste(tbl$index[1],"linear regression: slope =",paste(signif(coef$coefficients[2], digits=3),",",sep=""),
+                  "y Intercept =",paste(round(coef$coefficients[1], digits=3),",",sep=""),
+                  "r =",signif(r, digits=3))
+    plot(x=tbl$singlepred,y=tbl$refsamp,
+         main=title,
+         xlab=paste(tbl$oli_img[1],tbl$index[1]),
+         ylab=paste(tbl$ref_img[1],tbl$index[1]))
+    abline(coef = coef$coefficients, col="red")  
+    dev.off()
+    
+    #return the information
+    coef_tbl = data.frame(rbind(model$coefficients))
+    cnames = c("yint","b2c","b3c","b4c","b5c","b6c","b7c")
+    colnames(coef_tbl) = cnames
+    tbls = list(coef_tbl,tbl)
+    return(tbls)
+  }
 
 #   sample_it = function(img, bins, n){
 #     
